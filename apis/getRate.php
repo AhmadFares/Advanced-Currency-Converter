8d26ddc0d87f0397d696ef19e2496a8e97ddde29
Dana Ghaddar <dana.ghaddar@lau.edu>
2022-04-04 01:09:26 +0300

    curl

--- apis/getRate.php
+++ apis/getRate.php
@@ -5,9 +5,17 @@ $ch = curl_init('https://lirarate.org/');
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 $result = curl_exec($ch);
+$error = curl_error($ch);
+
+preg_match('/<strong id="buy-value">(.*)<\/strong>' , $result , $matches);
+
+print_r($matches);
+
+
+
 curl_close($ch);
 
-$dom = new DOMDocument;
+/*$dom = new DOMDocument;
 libxml_use_internal_errors($result);
 $dom->loadHTML($result);
 
@@ -16,5 +24,5 @@ $tags = $dom->getElementsByTagName('selection');
 foreach($tags as $code) {
 	echo $code->nodeValue;
 }
-
+*/
 ?>
